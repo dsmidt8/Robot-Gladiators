5492a5732a7416cbae99427e1355f4adad001fb2
David Smidt <dsmidt8@gmail.com>
2022-05-08 20:42:57 -0500

    fight added to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("this is an alert! JavaScript is running");
+var playerName=window.prompt("whats your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("our robot's name is" +playerName)
+function fight(){
+    window.alert("the fight has begun!")
+}
+//fight();
