785a0e349a9d147b081eda7a68a7116d2edaca1c
David Smidt <dsmidt8@gmail.com>
2022-05-09 18:28:28 -0500

    robot fight repeats

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,10 +11,12 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+
+    //repeat and execute as long as the enemy robot is alive
+ while(enemyHealth > 0){
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -47,7 +49,7 @@ var fight = function(enemyName) {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player choses to skip
-  } else if (promptFight === "skip" || promptFight === "SKIP") {
+   } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -65,6 +67,7 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+ }
 };
 
 // Game States
@@ -76,6 +79,8 @@ var fight = function(enemyName) {
     
  
   
-for(var i=0; i< enemyNames.length; i++){
-    fight(enemyNames[i]);
+for (var i=0; i < enemyNames.length; i++){
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
