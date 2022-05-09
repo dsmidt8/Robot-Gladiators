3c443b0b1ae5119c29fb3486d9920a985143e7fd
David Smidt <dsmidt8@gmail.com>
2022-05-09 08:10:39 -0500

    fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,36 @@
-var playerName=window.prompt("whats your robot's name?");
+var playerName = window.prompt("What's your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("this logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10+10);
 
-console.log("our robot's name is" +playerName)
-function fight(){
-    window.alert("the fight has begun!")
-}
-//fight();
+var fight = function() {
+
+    //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+    );
+
+    //subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth  - enemyAttack;
+
+    //log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+    );
+
+};
+
+//execute function
+fight();
